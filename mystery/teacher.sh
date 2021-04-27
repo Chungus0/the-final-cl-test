75dc8053a3b7688aaa705ea0bf1c16bf62f5e9b1
chungus0 <dguhanb@gmail.com>
2021-04-28 04:05:34 +0600

    please_work

--- mystery/teacher.sh
+++ mystery/teacher.sh
@@ -10,6 +10,6 @@ color=$(cat $( find -name '*699607') |rev|cut -d. -f1 | rev | cut -d, -f1| rev |
 nums=$(cat $( find -name '*699607') |rev|cut -d. -f1 | rev | cut -d, -f2| rev | cut -d' ' -f5|rev|cut -c 2-5)
 nume=$(cat $( find -name '*699607') |rev|cut -d. -f1 | rev | cut -d, -f2| rev | cut -d' ' -f1|rev|cut -c 2) 
 cd ..
-grep -A4 $nums "vehicles"|grep -A 4 $nume|grep -B 2 -A 2 -i $color|grep -B 1 -A 3 -i $make| grep -B 4 $height
+grep -A4 $nums vehicles|grep -A 4 $nume|grep -B 2 -A 2 -i $color|grep -B 1 -A 3 -i $make| grep -B 4 $height
 cd memberships
 cat "AAA" "Delta_SkyMiles" "Museum_of_Bash_History" "Terminal_City_Library"| grep -c "$MAIN_SUSPECT" 
