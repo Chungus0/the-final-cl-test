3c93b56279f8bea4806164e30c43142eade1e7f4
chungus0 <dguhanb@gmail.com>
2021-04-28 03:13:03 +0600

    please_work

---
+++ mystery/teacher.sh
@@ -0,0 +1,13 @@
+int_n=$( head -n 179 streets/Buckingham_Place|tail -n 1|grep -o '......$')
+echo $int_n
+cd interviews
+cat $( find -name '*699607')
+height=6\'
+make=$(cat $( find -name '*699607') |rev|cut -d. -f1|rev|cut -d, -f1|rev|cut -d' ' -f1|rev)
+color=$(cat $( find -name '*699607') |rev|cut -d. -f1 | rev | cut -d, -f1| rev | cut -d' ' -f2|rev)
+nums=$(cat $( find -name '*699607') |rev|cut -d. -f1 | rev | cut -d, -f2| rev | cut -d' ' -f5|rev|cut -c 2-5)
+nume=$(cat $( find -name '*699607') |rev|cut -d. -f1 | rev | cut -d, -f2| rev | cut -d' ' -f1|rev|cut -c 2) 
+cd ..
+grep -A4 $nums "vehicles"|grep -A4 $nume|grep -B2 -A2 -i $color|grep -B1 -A3 -i $make| grep -B 4 $height
+cd memberships
+cat "AAA" "Delta_SkyMiles" "Museum_of_Bash_History" "Terminal_City_Library"| grep -c "MAIN_SUSPECT"
